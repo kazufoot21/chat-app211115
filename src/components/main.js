38d903c24665db4d82700b7cf88aa98838779cda
kazufoot21 <kazuhiromaeda@maedatomohironoMacBook-Pro.local>
2021-11-17 09:56:06 +0900

    apply agird layout

--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {makeStyles} from '@mui/styles';
+import MessageInputField from './Message.InputField';
+import MessageList from './MessageList';
 
-const Main = () => {
-    return <div>main</div>;
+const useStyles = makeStyles({
+    root: {
+        display: 'grid',
+        height: '100vh',
+        gridTemplateRows: '1fr auto',
+    },
+});
+
+const Main = ({name}) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <MessageList />
+            <MessageInputField name={name} />
+        </div>
+    );
 };
 
 export default Main;

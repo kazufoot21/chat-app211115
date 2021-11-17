d88ff4bad0db48a88402c59ea6ae259d491cd4cd
kazufoot21 <kazuhiromaeda@maedatomohironoMacBook-Pro.local>
2021-11-17 17:22:42 +0900

    remove hard corded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,21 @@
 import firebase from 'firebase';
+
+const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-    apiKey: 'AIzaSyAxLR_bYrDoCh2RxdfvUJPBnV9Qt_g7ln0',
-    authDomain: 'chat-app-3458a.firebaseapp.com',
-    projectId: 'chat-app-3458a',
-    storageBucket: 'chat-app-3458a.appspot.com',
-    messagingSenderId: '930920722824',
-    appId: '1:930920722824:web:78aa5e256ecc33d8728a3d',
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig); //初期化

f3993439514096e6e58bba131f3cbdbc282055ad
kazufoot21 <kazuhiromaeda@maedatomohironoMacBook-Pro.local>
2021-11-17 11:09:12 +0900

    generate avatar

--- src/components/Message.InputField.js
+++ src/components/Message.InputField.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {makeStyles} from '@mui/styles';
 import {Grid, Avatar} from '@mui/material';
+import {gravatarPath} from '../gravatar';
 
 const useStyles = makeStyles({
     root: {
@@ -9,13 +10,15 @@ const useStyles = makeStyles({
     },
 });
 
-const MessageInputField = () => {
+const MessageInputField = ({name}) => {
     const classes = useStyles();
+    const avatarPath = gravatarPath(name); //'kazufoot21@outlook.jp'
+
     return (
         <div className={classes.root}>
             <Grid container>
                 <Grid xs={1}>
-                    <Avatar />
+                    <Avatar src={avatarPath} />
                 </Grid>
                 <Grid xs={10}>input</Grid>
                 <Grid xs={1}>button</Grid>
